3ceaf88c7e80980c91fa9def98137ced0eaaf660
pdpapper <prahlada@berkeley.edu>
2017-10-05 13:21:30 -0700

    Update source_files.r
    Migrate to dackerly project master

--- source_files.r
+++ source_files.r
@@ -1,9 +1,9 @@
 
 # scripts related to the 50 original veg plots and env data
-source("https://raw.githubusercontent.com/pdpapper/PepperwoodVegPlots/patch-2/Analyses/PWfunctions_GitHub.R")
+source("https://raw.githubusercontent.com/dackerly/PepperwoodVegPlots/master/Analyses/PWfunctions_GitHub.R")
 
 # scripts for the 17 larger hectare tree plots
-source("https://raw.githubusercontent.com/pdpapper/PepperwoodVegPlots/patch-2/2016/Hectares/hectare_scripts.r")
+source("https://raw.githubusercontent.com/dackerly/PepperwoodVegPlots/master/2016/Hectares/hectare_scripts.r")
 
 # scripts for working with HOBO data
-source("https://raw.githubusercontent.com/pdpapper/PepperwoodVegPlots/patch-2/HOBO/get_hobos.r")
+source("https://raw.githubusercontent.com/dackerly/PepperwoodVegPlots/master/HOBO/get_hobos.r")
